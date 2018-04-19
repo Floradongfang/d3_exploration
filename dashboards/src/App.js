c7ed2b48c67057bd8155b401a3c52b30f706f06b
MattTriano <matt.triano@gmail.com>
2018-04-19 08:14:31 -0500

    added a map component to my dashboard

--- dashboards/src/App.js
+++ dashboards/src/App.js
@@ -1,16 +1,39 @@
 import React, { Component } from 'react';
 import './App.css';
 import BarChart from './BarChart'
+import worlddata from './world'
+import WorldMap from './WorldMap'
+import { range, sum } from 'd3-array'
+import { scaleThreshold } from 'd3-scale'
+import { geoCentroid } from 'd3-geo'
+
+const appdata = worlddata.features
+  .filter(d => geoCentroid(d)[0] <- -20)
+
+appdata
+  .forEach((d,i) => {
+    const offset = Math.random()
+    d.launchday = i
+    d.data = range(30).map((p,q) => 
+      q < i ? 0 : Math.random() * 2 + offset)
+  })
 
 class App extends Component {
   render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <h2>d3ia dashboard</h2>
-        </div>
+    const colorScale = scaleThreshold()
+      .domain([5,10,20,30,50])
+      .range(["#99d8c9","#66c2a4","#41ae76","#238b45","#005824"])
+      return (
+        <div className="App">
+          <div className="App-header">
+            <h2>d3ia dashboard</h2>
+          </div>
         <div>
-          <BarChart data={[5,8,1,3]} size={[500,500]} />
+          // Cool, we can pass data directly to a custom-built visualization
+          //   in an html element's tag
+          // <BarChart data={[5,10,1,3]} size={[500,500]} />
+
+          <WorldMap colorScale={colorScale} data={appdata} size={[500,400]} />
         </div>
         
       </div>
