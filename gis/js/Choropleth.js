aa2cfecc0a0121d464edf482ce75b1a5e266e83b
MattTriano <matt.triano@gmail.com>
2018-05-06 19:51:39 -0500

    got a sample data set from the 5 year ACS to work with, and made a preprocessor notebook

--- gis/js/Choropleth.js
+++ gis/js/Choropleth.js
@@ -5,7 +5,9 @@ class Choropleth {
 		this.geojson = geojson;
 		this.outline = outline;
 
+		this.dispatch = d3.dispatch('click', 'hover');
 		this.map = d3.geoPath().projection(d3.geoAlbersUsa());
+		this.legend = this.svg.append('g').attr('transform', `translate(280,15)`);
 
 		// The term 'counties' essentially means 'polygon'
 		this.counties = this.svg.append('g');
@@ -14,22 +16,37 @@ class Choropleth {
 			.data(this.geojson.features)
 			.enter()
 			.append('path')
-				.attr('d', this.map);
-				// .classed('feature', true);
+				.attr('d', this.map)
+				.classed('feature', true)
+				.on('mouseover', (d, i) => self.dispatch
+					.call('hover', {}, d.properties.GEOID, i));
 
+		console.log("this.counties: ");
 		console.log(this.counties);
+		console.log(this.geojson.features);
 
-		this.counties
-			.data(this.outline.features)
-			.append('path')
-			.attr(this.map)
-			// .classed('outline', true)
-			// .lower()
+		// this.counties
+		// 	.data(this.outline.features)
+		// 	.append('path')
+		// 	.attr(this.map)
+		// 	// .classed('outline', true)
+		// 	// .lower()
 	}
 
-	// update() {
-	// 	const self = this;
-	// 	this.counties.selectAll('path').classed
+	update() {
+		const self = this;
+		this.state = state;
+		this.counties.selectAll('path').classed('active', function (d, i) {
+				if (state.selected[d.properties.GEOID]) { d3.select(this).raise(); }
+				state,selected[d.properties.GEOID];
+			})
+			.transition()
+			.duration(50)
+			.attr('fill', '#2ca25f');
 
-	// }
+	}
+	hover(state) {
+		this.counties.selectAll('path').classed('hover', (d, i) => 
+			d.properties.GEOID === state.hover);
+	}
 }
--- gis/js/Colorer.js
+++ gis/js/Colorer.js
@@ -1,5 +1,22 @@
 class Colorer {
-	constructor() {
-	
+	constructor(indicator_values) {
+		this.indicator_values = indicator_values;
+
 	}
+
+	getMax(indicator) {
+		return Math.max.apply(null, Object
+			.keys(this.indicator_values[indicator])
+			.map(key => this.indicator_values[indicator][key])
+			.filter(v => !!v));
+	}
+
+	getMin(indicator) {
+		return Math.min.apply(null, Object
+			.keys(this.indicator_values[indicator])
+			.map(key => this.indicator_values[indicator][key])
+			.filter(v => !!v));
+	}
+
+
 }
